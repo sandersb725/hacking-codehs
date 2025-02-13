dcde1f895874a71178f528c6920dcbbed3063e58
sandersb725 <136634756+sandersb725@users.noreply.github.com>
2025-02-13 10:16:04 -0500

    Create firefox.py
    Run firefox on codehs.com (OPTION "Display Server Graphics Screen" MUST BE ON)

---
+++ examples/firefox.py
@@ -0,0 +1,7 @@
+import os 
+
+os.system('''wget https://ftp.mozilla.org/pub/firefox/releases/131.0b9/linux-x86_64/en-US/firefox-131.0b9.tar.bz2
+tar -xvf firefox-131.0b9.tar.bz2
+cd firefox
+./firefox
+''')
