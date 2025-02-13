af31efcbd4cbb442d215ceb1d2f6bbd6263c11e3
sandersb725 <136634756+sandersb725@users.noreply.github.com>
2025-02-13 10:21:15 -0500

    Create pycharm.py
    Run PyCharm on codehs.com

---
+++ examples/pycharm.py
@@ -0,0 +1,7 @@
+import os
+
+os.system('''
+wget https://download.jetbrains.com/python/pycharm-professional-2024.2.3.tar.gz
+tar -xvf pycharm-professional-2024.2.3.tar.gz
+cd pycharm-2024.2.3/bin
+./pycharm.sh''')
