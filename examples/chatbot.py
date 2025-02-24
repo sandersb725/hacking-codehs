809654b72b4ef2db54ca986604ae6ef477352f11
Brighton Sanders <136634756+sandersb725@users.noreply.github.com>
2025-02-24 10:26:05 -0500

    Update chatbot.py

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -6,8 +6,8 @@ import base64
 from hugchat import hugchat
 from hugchat.login import Login
 
-EMAIL = "bus120610@gmail.com"
-PASSWD = base64.b64decode("TiZZUHIza1dyKDZkVlNt".encode("ascii")).decode("ascii")
+EMAIL = "EMAIL"
+PASSWD = "PASSWD"
 cookie_path_dir = "./cookies/"
 sign = Login(EMAIL, PASSWD)
 cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
